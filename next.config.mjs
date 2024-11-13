25d31f4688405a5174a653e8a5a0e0972d6c5bf6
19alexandr80 <usanka1980@gmail.com>
2024-11-13 07:48:01 +0200

    gggfff

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { base: "/{19alexandr80.github.io/numerorum/}" };
+const nextConfig = { base: "https://19alexandr80.github.io/numerorum/" };
 
 export default nextConfig;
