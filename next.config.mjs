5ddb81a6aebc1cda3b0665c461309e8f29de8647
19alexandr80 <usanka1980@gmail.com>
2024-11-13 07:21:23 +0200

    noRa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { base: "/{nemerorum}", plugins: [React()] };
+const nextConfig = { base: "/{nemerorum}" };
 
 export default nextConfig;
