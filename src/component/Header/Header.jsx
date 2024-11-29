92e30ed2ff7b857197c1f271601307486037dd4c
19alexandr80 <usanka1980@gmail.com>
2024-11-29 05:31:23 +0200

    bbq

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -6,7 +6,7 @@ import style from "./header.module.css";
 export default function Header() {
   return (
     <>
-      <div className={`${style.header} ${style.page}`}>
+      <div className={`${style.header} `}>
         <Link className="ctas" href="/">
           Home
         </Link>
